b54a5f64b74da15b6ccecc216dd9c309b4a836e3
mrTkach <mr.tkach.a@gmail.com>
2019-01-28 10:33:21 +0200

    first commit

---
+++ mail.php
@@ -0,0 +1,32 @@
+﻿<?php
+	/* Задаем переменные */
+	$name = htmlspecialchars($_POST["name"]);
+	$tel = htmlspecialchars($_POST["tel"]);
+	$noSpam = htmlspecialchars($_POST["noSpam"]);
+	 
+	/* Мой адрес и тема сообщения */
+	$address = "mr.tkach.a@gmail.com";
+	$sub = "Сообщение с моего сайта";
+	 
+	/* Формат письма */
+	$mes = "Сообщение с моего сайта.\n
+	Имя отправителя: $name\n
+	Телефон отправителя: $tel";
+	 
+	 
+	if (empty($noSpam)) {
+		// Оценка поля noSpam - должно быть пустым
+		// Отправляю сообщение, используя mail() функцию
+	$from  = "From: $name <$tel> \r\n";
+	if (mail($address, $sub, $mes, $from)) { 
+		// header("location: thanks.html");
+		echo '
+	    	<div>Заказ отправлен! Оставайтесь на связи.
+	    	</div>'
+	} else {
+	 	echo '
+	 		<div>Что-то пошло не так. Попробуйте ещё раз.</div>'
+	 	}
+	}
+	exit; /* Выход без сообщения, если поле noSpam заполнено спам ботами */
+?>
